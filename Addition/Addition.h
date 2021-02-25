894e71be3d58b4901a6b4b613f74072c5f796c9f
Ashish Mishra <ashish.mishra@wynk.in>
2021-02-26 01:13:35 +0530

    initial commit

---
+++ Addition/Addition.h
@@ -0,0 +1,18 @@
+//
+//  Addition.h
+//  Addition
+//
+//  Created by B0212150 on 25/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Addition.
+FOUNDATION_EXPORT double AdditionVersionNumber;
+
+//! Project version string for Addition.
+FOUNDATION_EXPORT const unsigned char AdditionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Addition/PublicHeader.h>
+
+
---
+++ Addition/add.swift
@@ -0,0 +1,15 @@
+//
+//  add.swift
+//  Addition
+//
+//  Created by B0212150 on 25/02/21.
+//
+
+import Foundation
+
+public class Addition {
+
+    public func addTwoNumbers(a: Int, b: Int) -> Int {
+        return (a+b)
+    }
+}
